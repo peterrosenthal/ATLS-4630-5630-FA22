b40218343bfd547a16428c80154cdc86806c165d
Peter Rosenthal <peter@rosenth.al>
2022-09-15 10:21:01 -0600

    update forms example to include preventDefault

--- 09-13/main.js
+++ 09-13/main.js
@@ -18,3 +18,9 @@ button.addEventListener('click', () => {
     console.log(checkbox.checked);
   }
 });
+
+// disable form from refreshing the page when you press enter
+const myForm = document.getElementById('my-form');
+myForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
